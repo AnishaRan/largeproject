7bb9c1b769692065ade50cec2917e60b8d447018
Anisha Ranjan <72667580+AnishaRan@users.noreply.github.com>
2023-03-24 16:00:34 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from './pages/LoginPage';
+import CardPage from './pages/CardPage';
+import RegisterPage from './pages/RegisterPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" index element={<LoginPage />} />
+      <Route path="/cards" index element={<CardPage />} />
+      <Route path="/register" index element={<RegisterPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+}
+
+export default App;
